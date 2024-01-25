8cef07116841a808d2023712fd8c4eb9c5a1f8a1
doyoungkim97 <eh4536@naver.com>
2024-01-25 23:55:57 +0900

    solution : 백준 기초 체스문제 (2024/01/25)

---
+++ AlgorithmPractice/src/체스기초.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class 체스기초 {
+    public static void main(String[] args) {
+        int[] chessSets = {1,1,2,2,2,8};
+        String answer = new String();
+        Scanner sc = new Scanner(System.in);
+        String currentChess = sc.nextLine();
+
+        String[] chesses = currentChess.split(" ");
+        for(int i=0; i<chessSets.length; i++){
+            answer += Integer.toString(chessSets[i] - Integer.parseInt(chesses[i]));
+            answer += " ";
+        }
+        System.out.println(answer);
+    }
+
+    public static int stringToInt(String number){
+        return Integer.parseInt(number);
+    }
+}
