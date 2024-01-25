3e70bb1efb8707601d297fe4024168d7947dc854
doyoungkim97 <eh4536@naver.com>
2024-01-26 00:20:26 +0900

    solution : 백준 기초 팰린드롬 (2024/01/26)

---
+++ src/백준심화1/팰린드롬.java
@@ -0,0 +1,21 @@
+package 백준심화1;
+
+import java.util.Scanner;
+
+public class 팰린드롬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String words = sc.next();
+
+        int mid = words.length() / 2;
+        int end = words.length()-1;
+        int result = 1;
+        for(int i=0; i<mid; i++){
+            if(words.charAt(i) != words.charAt(end-i)) {
+                result = 0;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
